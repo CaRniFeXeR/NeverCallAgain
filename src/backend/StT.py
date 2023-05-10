cd3120918935321a6a7192c629c4b09ccf520a5a
Florian Kowarsch <florian.kowarsch@gmail.com>
2023-05-10 11:21:20 +0200

    adds speech to text class

---
+++ src/backend/StT.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+from transformers import pipeline
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class SpeechToText():
+
+    def __init__(self, fs = 16000) -> None:
+        self.fs = fs
+        #either use whisper small or medium
+        self.model_pipe = pipeline("automatic-speech-recognition", model="bofenghuang/whisper-small-cv11-german", device=device)
+    
+    def speech_to_text(self, audio: np.ndarray):
+        with torch.no_grad():
+            generated_sentences = self.model_pipe(audio)["text"]
+        
+        return generated_sentences
