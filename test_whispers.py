084ba19176e30c419bb4badb860e9607360ae081
Florian Kowarsch <florian.kowarsch@gmail.com>
2023-05-12 18:02:39 +0200

    updates whsipers test file

--- test_whispers.py
+++ test_whispers.py
@@ -1,20 +1,18 @@
+import time
 import torch
 
-from datasets import load_dataset
+# from datasets import load_dataset
 from transformers import pipeline
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Load pipeline
-pipe = pipeline("automatic-speech-recognition", model="bofenghuang/whisper-large-v2-cv11-german", device=device)
+#big, small etc
+pipe = pipeline("automatic-speech-recognition", model="bofenghuang/whisper-small-cv11-german", device=device)
 
 # NB: set forced_decoder_ids for generation utils
 pipe.model.config.forced_decoder_ids = pipe.tokenizer.get_decoder_prompt_ids(language="de", task="transcribe")
-
-# Load data
-ds_mcv_test = load_dataset("mozilla-foundation/common_voice_11_0", "de", split="test", streaming=True)
-test_segment = next(iter(ds_mcv_test))
-waveform = test_segment["audio"]
+pipe.model.eval()
 
 # NB: decoding option
 # limit the maximum number of generated tokens to 225
@@ -28,7 +26,19 @@ pipe.model.config.max_length = 225 + 1
 # pipe.model.config.output_scores = True
 # pipe.model.config.num_return_sequences = 5
 
+start_time = time.time()
+
+import soundfile as sf
+# load wave file from disk
+waveform, sr = sf.read("out.wav")
+
 # Run
-generated_sentences = pipe(waveform)["text"]
+with torch.no_grad():
+    generated_sentences = pipe(waveform)["text"]
 
 print(generated_sentences)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Elapsed time: {elapsed_time:.5f} seconds")
