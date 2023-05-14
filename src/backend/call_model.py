e0a310dcb4c8ad1ec7e3d2de7b986fe4763c37f0
Florian Pfiel <fpfiel@gmx.net>
2023-05-14 08:30:43 +0200

    backend call_model

---
+++ src/backend/call_model.py
@@ -0,0 +1,21 @@
+class Call:
+    def init(
+        self,
+        title,
+        state,
+        receiverName,
+        receiverPhonenr,
+        initiatorName,
+        possibleDatetimes,
+        result=None,
+    ):
+        self.title = title
+        self.state = state
+        self.receiverName = receiverName
+        self.receiverPhonenr = receiverPhonenr
+        self.initiatorName = initiatorName
+        self.possibleDatetimes = possibleDatetimes
+        self.result = result
+
+    def setResult(self, result):
+        self.result = result
